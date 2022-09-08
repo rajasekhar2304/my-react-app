13a4e8eaa2d5b52490282843361fec09c1767414
rajasekhar <raajasekharr@gmail.com>
2022-09-08 14:34:06 +0530

    Modified h1 in App.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ function App() {
 	return (
     <div className="App">
 	<h1>Hello World! This is  Gurugubelli Rajasekhar</h1>
-  <h1>Hello World! This is Ravikanth</h1>		
-      <header className="App-header">
+        <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           DevRabbit offers you SmartQE to make Testing and Deployment easy.
