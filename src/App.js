7e7c1d16f9790641b2165edb3fd07ab700692405
rajasekhar <raajasekharr@gmail.com>
2022-09-08 17:36:10 +0530

    modified the App.js file

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
 	return (
     <div className="App">
 	<h1>Hello World! This is  Gurugubelli Rajasekhar</h1>
-        <p>Suite America Demo</p>
+        <p>Suite America Demo project</p>
 	<header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
